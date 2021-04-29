ac943d53d61f0d0e919b674a65e70cbc3d927347
tungdao17 <tungdao17@gmail.com>
2021-04-29 23:07:28 +0800

    Update the title and description

--- site.config.js
+++ site.config.js
@@ -2,8 +2,8 @@ const categories = require('./src/data/categories');
 
 module.exports = {
   site: {
-    title: 'AI Engineering',
-    description: 'AI Engineering Blog',
+    title: 'AI Engineer',
+    description: 'AI Engineer Blog',
     basePath: process.env.NODE_ENV === 'production' ? '' : '',
     categories,
   },
